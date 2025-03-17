0951b0741e1c3af84d03af21b9d93152b8f76d20
Dan Edwards <daniel.edwards96@yahoo.com>
2025-03-17 19:46:13 +0000

    Delete getSlugFromDisplay.ts

--- src/library/getSlugFromDisplay.ts
+++
@@ -1,8 +0,0 @@
-import type { WriterDisplayName, WriterSlug } from '@/types'
-import { writerDetails } from './constants'
-
-export function getSlugFromDisplay(displayName: WriterDisplayName): WriterSlug | undefined {
-	const entries = Object.entries(writerDetails) as [WriterSlug, WriterDisplayName][]
-	const entry = entries.find(([_, value]) => value === displayName)
-	return entry ? entry[0] : undefined
-}
