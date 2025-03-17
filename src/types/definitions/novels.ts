b35f2858f6ab10bdab2a08ec5c3fceee5e435ad9
Dan Edwards <daniel.edwards96@yahoo.com>
2025-03-17 16:05:53 +0000

    Restructure novels data

--- src/types/definitions/novels.ts
+++ src/types/definitions/novels.ts
@@ -1,12 +1,16 @@
 import type { novels } from '@/library/constants'
 import type { WriterDisplayName } from './writer'
 
+interface Chapter {
+	title?: string
+	content: string[]
+}
+
 export interface Novel {
 	slug: NovelSlug | 'change or error'
 	title: string
 	writer: WriterDisplayName
-	chapters: Array<Array<string>>
+	chapters: Chapter[]
 }
 
 export type NovelSlug = (typeof novels)[keyof typeof novels]
-// export type NovelSlugKey = keyof typeof novels
