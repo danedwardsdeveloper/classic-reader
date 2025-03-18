a894320bbe20b7d856cbc8bc0e5e761f9439d721
Dan Edwards <daniel.edwards96@yahoo.com>
2025-03-18 17:09:56 +0000

    Create extractedEpubContentToIgnore.ts

---
+++ extractedEpubContentToIgnore.ts
@@ -0,0 +1,42 @@
+const _extractedEpubContentToIgnore = [
+	{
+		content: ['By L. M. Montgomery.'],
+	},
+	{
+		content: [
+			'This ebook is the product of many hours of hard work by volunteers for Standard Ebooks, and builds on the hard work of other literature lovers made possible by the public domain.',
+			'This particular ebook is based on a transcription from Faded Page Canada and on digital scans from the Internet Archive.',
+			'The source text and artwork in this ebook are believed to be in the United States public domain; that is, they are believed to be free of copyright restrictions in the United States. They may still be copyrighted in other countries, so users located outside of the United States must check their local laws before using this ebook. The creators of, and contributors to, this ebook dedicate their contributions to the worldwide public domain via the terms in the CC0 1.0 Universal Public Domain Dedication. For full license information, see the Uncopyright at the end of this ebook.',
+			'Standard Ebooks is a volunteer-driven project that produces ebook editions of public domain literature using modern typography, technology, and editorial standards, and distributes them free of cost. You can download this and other ebooks carefully produced for true book lovers at standardebooks.org.',
+		],
+	},
+	{
+		content: ['To\n\t\t\tall the girls all over the world\n\t\t\twho have “wanted more” about\n\t\t\tAnne'],
+	},
+	{
+		content: [
+			'“All precious things discovered late\n\t\t\t\t\t\n\t\t\t\t\tTo those that seek them issue forth,\n\t\t\t\t\t\n\t\t\t\t\tFor Love in sequel works with Fate,\n\t\t\t\t\t\n\t\t\t\t\tAnd draws the veil from hidden worth.”',
+		],
+	},
+	// Actual book content...
+	{
+		content: ['Chronicles of Avonlea. ↩︎'],
+	},
+	{
+		content: [
+			'Anne of the Island\n\t\t\twas published in 1915 by\n\t\t\tL. M. Montgomery.',
+			'This ebook was produced for\n\t\t\tStandard Ebooks\n\t\t\tby\n\t\t\tGwen Byrne,\n\t\t\tand is based on a transcription produced in 2012 by\n\t\t\tDavid Edwards, Wojtek Murawski, and The Online Distributed Proofreading Canada Team\n\t\t\tfor\n\t\t\tFaded Page Canada\n\t\t\tand on digital scans from the\n\t\t\tInternet Archive.',
+			'The cover page is adapted from\n\t\t\tMy Daughter Elisabeth,\n\t\t\ta painting completed circa 1914 by\n\t\t\tFrank Weston Benson.\n\t\t\tThe cover and title pages feature the\n\t\t\tLeague Spartan and Sorts Mill Goudy\n\t\t\ttypefaces created in 2014 and 2009 by\n\t\t\tThe League of Moveable Type.',
+			'This edition was released on\n\t\t\tNovember 7, 2024, 8:56 p.m.\n\t\t\tand is based on\n\t\t\trevision 24cb031.\n\t\t\tThe first edition of this ebook was released on\n\t\t\tMay 2, 2024, 6:58 p.m.\n\t\t\tYou can check for updates to this ebook, view its revision history, or download it for different ereading systems at\n\t\t\tstandardebooks.org/ebooks/l-m-montgomery/anne-of-the-island.',
+			'The volunteer-driven Standard Ebooks project relies on readers like you to submit typos, corrections, and other improvements. Anyone can contribute at standardebooks.org.',
+		],
+	},
+	{
+		content: [
+			'May you do good and not evil.\n\t\t\t\t\t\n\t\t\t\t\tMay you find forgiveness for yourself and forgive others.\n\t\t\t\t\t\n\t\t\t\t\tMay you share freely, never taking more than you give.',
+			'Copyright pages exist to tell you that you can’t do something. Unlike them, this Uncopyright page exists to tell you that the writing and artwork in this ebook are believed to be in the United States public domain; that is, they are believed to be free of copyright restrictions in the United States. The United States public domain represents our collective cultural heritage, and items in it are free for anyone in the United States to do almost anything at all with, without having to get permission.',
+			'Copyright laws are different all over the world, and the source text or artwork in this ebook may still be copyrighted in other countries. If you’re not located in the United States, you must check your local laws before using this ebook. Standard Ebooks makes no representations regarding the copyright status of the source text or artwork in this ebook in any country other than the United States.',
+			'Non-authorship activities performed on items that are in the public domain﻿—so-called “sweat of the brow” work﻿—don’t create a new copyright. That means that nobody can claim a new copyright on an item that is in the public domain for, among other things, work like digitization, markup, or typography. Regardless, the contributors to this ebook release their contributions under the terms in the CC0 1.0 Universal Public Domain Dedication, thus dedicating to the worldwide public domain all of the work they’ve done on this ebook, including but not limited to metadata, the titlepage, imprint, colophon, this Uncopyright, and any changes or enhancements to, or markup on, the original text and artwork. This dedication doesn’t change the copyright status of the source text or artwork. We make this dedication in the interest of enriching our global cultural heritage, to promote free and libre culture around the world, and to give back to the unrestricted culture that has given all of us so much.',
+		],
+	},
+]
