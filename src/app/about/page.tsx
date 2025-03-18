e22f24d51e98a1983cc6ee7b279ac53ad1ae559c
Dan Edwards <daniel.edwards96@yahoo.com>
2025-03-18 15:28:55 +0000

    Delete secondary navigation

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,5 @@
 import BreadCrumbs from '@/components/BreadCrumbs'
 import Footer from '@/components/Footer'
-import { SecondaryNavigation } from '@/components/SecondaryNavigation'
 import { metaTitleTemplate } from '@/library/environment'
 import { CheckCircleIcon } from '@heroicons/react/24/outline'
 import type { Metadata } from 'next'
@@ -25,16 +24,19 @@ export default function AboutPage() {
 			<div className="flex-1 mb-20">
 				<BreadCrumbs currentPageName="About" />
 				<h1>About</h1>
-				<ul className="flex flex-col gap-y-5 text-lg">
+				<p className="mb-4 text-zinc-800 text-lg">
+					ClassicReader.org is a website that allows you to read classic novels in the browser without any distractions.
+				</p>
+				<p className="mb-4 text-zinc-800 text-lg">{`It's completely free and sustained solely by generous donations from readers like you.`}</p>
+				<ul className="flex flex-col gap-y-5 text-lg text-zinc-800">
 					{bullets.map((bullet) => (
 						<li key={bullet} className="flex gap-x-2 items-start">
-							<CheckCircleIcon className="size-6 text-emerald-500 shrink-0 mt-1" />
+							<CheckCircleIcon className="size-6 text-emerald-600 shrink-0 mt-1" />
 							{bullet}
 						</li>
 					))}
 				</ul>
 			</div>
-			<SecondaryNavigation />
 			<Footer />
 		</>
 	)
