284fb29f4bceefa9cd06e40269d6a0ae3d4280f5
Dan Edwards <daniel.edwards96@yahoo.com>
2025-03-19 10:17:50 +0000

    Styling overhaul

--- src/components/NovelsList.tsx
+++ src/components/NovelsList.tsx
@@ -1,55 +1,103 @@
 'use client'
-import { generateChapterPath, generateNovelPath } from '@/library/utilities/client/definitions/generatePaths'
+import { generateChapterPath, generateNovelPath, generateWriterPath } from '@/library/utilities/client/definitions/generatePaths'
 import { useLocalStorage } from '@/providers/localStorage'
 import type { Novel } from '@/types'
+import { ArrowRightCircleIcon, CheckCircleIcon } from '@heroicons/react/24/outline'
 import clsx from 'clsx'
 import Link from 'next/link'
+import AnimatedEllipsis from './AnimatedEllipsis'
 
 export default function NovelsList({ novels }: { novels: Novel[] }) {
-	const { getBookChapters, getNovelProgress, getNextUnreadChapter } = useLocalStorage()
+	const { getBookChapters, getNovelProgress, getNextUnreadChapter, isLoading } = useLocalStorage()
+
+	function DynamicChapterLink({
+		isFinished,
+		hasProgress,
+		nextChapterPath,
+		nextUnreadChapterNumber,
+	}: { isFinished: boolean; hasProgress: boolean; nextChapterPath: string; nextUnreadChapterNumber: number | null }) {
+		if (isLoading)
+			return (
+				<p className="text-zinc-600 italic">
+					Loading progress
+					<AnimatedEllipsis />
+				</p>
+			)
+
+		if (isFinished)
+			return (
+				<div className="text-green-600 flex items-center gap-x-2">
+					<p>Finished</p>
+					<CheckCircleIcon className="size-5" />
+				</div>
+			)
+		if (hasProgress) {
+			return (
+				<Link
+					href={nextChapterPath}
+					className="flex items-center gap-x-2 link text-orange-700 hover:text-orange-600 active:text-orange-500"
+				>
+					<p>Continue at chapter {nextUnreadChapterNumber}</p>
+					<ArrowRightCircleIcon className="size-5" />
+				</Link>
+			)
+		}
+
+		return (
+			<Link href={nextChapterPath} className="link text-blue-600 hover:text-blue-500 active:text-blue-400">
+				Start reading
+			</Link>
+		)
+	}
 
 	return (
-		<ul className="flex flex-col gap-y-8 divide-y-2 divide-zinc-100">
-			{novels.map((novelData, index) => {
+		<ul className="flex flex-col gap-y-14 divide-y-2 divide-zinc-100">
+			{novels.map((novelData) => {
 				if (!novelData) return null
 
 				const localStorageData = getBookChapters(novelData.titleSlug)
 				const hasProgress = getNovelProgress(localStorageData)
-				const nextUnreadChapter = getNextUnreadChapter(novelData.titleSlug, novelData.chapters.length)
+
+				const nextUnreadChapterNumber = getNextUnreadChapter(novelData.titleSlug, novelData.chapters.length)
+
 				const nextChapterPath = generateChapterPath({
 					novelData: novelData,
-					oneIndexedChapterNumber: nextUnreadChapter || 1,
+					oneIndexedChapterNumber: nextUnreadChapterNumber || 1,
 				})
-				const progressPath = generateNovelPath(novelData)
-				const isFinished = nextUnreadChapter === null
+
+				const isFinished = nextUnreadChapterNumber === null
+
+				const overviewPath = generateNovelPath(novelData)
+				const writerPath = generateWriterPath(novelData.writerSlug)
 
 				return (
 					<li key={novelData.titleSlug}>
-						<div className={clsx('flex flex-col gap-y-2 w-full', index !== 0 && 'mt-6')}>
-							<div className="flex w-full justify-between">
-								<span className="text-xl text-zinc-800 font-medium">{novelData.titleDisplay}</span>
-								<span className="text-lg text-zinc-700">{novelData.writerDisplay}</span>
-							</div>
-							<div className="flex w-full justify-between">
-								{isFinished ? (
-									<p className="text-green-600">Finished</p>
-								) : (
-									<Link
-										href={nextChapterPath}
-										className={clsx(
-											'link',
-											hasProgress
-												? 'text-orange-600 hover:text-orange-500 active:text-orange-400'
-												: 'text-blue-600 hover:text-blue-500 active:text-blue-400',
-										)}
-									>
-										{hasProgress ? `Continue at chapter ${nextUnreadChapter}` : 'Read...'}
-									</Link>
-								)}
-								<Link href={progressPath} className="link-primary">
-									Manage progress
-								</Link>
-							</div>
+						<div
+							className={clsx(
+								'relative pl-5 mt-10',
+								'before:absolute before:left-0 before:top-0 before:bottom-0 before:w-1 before:rounded before:transition-all before:duration-300',
+								isFinished
+									? 'before:bg-green-500 before:group-hover:bg-green-600'
+									: hasProgress
+										? 'before:bg-amber-500 before:group-hover:bg-amber-600'
+										: 'before:bg-gray-200 before:group-hover:bg-gray-300',
+							)}
+						>
+							<Link href={overviewPath} className="block text-xl text-zinc-800 font-medium link hover:opacity-90 active:opacity-80">
+								{novelData.titleDisplay}
+							</Link>
+							<Link href={writerPath} className="block text-lg text-zinc-800 mb-4 link hover:opacity-90 active:opacity-80">
+								{novelData.writerDisplay}
+							</Link>
+							<Link href={overviewPath} className="block link-primary mb-1">
+								Overview
+							</Link>
+							<DynamicChapterLink
+								hasProgress={hasProgress}
+								nextChapterPath={nextChapterPath}
+								isFinished={isFinished}
+								nextUnreadChapterNumber={nextUnreadChapterNumber}
+							/>
 						</div>
 					</li>
 				)
