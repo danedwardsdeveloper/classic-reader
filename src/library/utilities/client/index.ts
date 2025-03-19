a20dcf51af43c362cd05c06c6060f269a2891900
Dan Edwards <daniel.edwards96@yahoo.com>
2025-03-19 10:17:03 +0000

    Remove unused utilities

--- src/library/utilities/client/index.ts
+++ src/library/utilities/client/index.ts
@@ -1,7 +1,4 @@
-import { oxford as formatList } from 'humanize-plus'
 export * from './definitions/generatePaths'
 export * from './definitions/getWriterDisplayName'
 export * from './definitions/isWriterSlug'
 export * from './definitions/hasProgress'
-
-export { formatList }
