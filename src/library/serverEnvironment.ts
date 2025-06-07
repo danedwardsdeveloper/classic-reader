5e0a562369741d92f66409e42e4bdc1d8fff21e3
Dan Edwards <daniel.edwards96@yahoo.com>
2025-06-07 21:46:54 +0100

    Fix serverSideBaseUrl

--- src/library/serverEnvironment.ts
+++ src/library/serverEnvironment.ts
@@ -1,3 +1,7 @@
 import { developmentBaseURL, isProduction, productionBaseURL } from './environment'
 
-export const serverSideBaseUrl = isProduction ? (process.env.VERCEL_URL ?? productionBaseURL) : developmentBaseURL
+export const serverSideBaseUrl = isProduction
+	? process.env.VERCEL_URL
+		? `https://${process.env.VERCEL_URL}`
+		: productionBaseURL
+	: developmentBaseURL
