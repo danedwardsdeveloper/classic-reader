e3ad480d778407dab18a1837a007059b8cd5e047
Dan Edwards <daniel.edwards96@yahoo.com>
2025-06-07 21:14:08 +0100

    Add metadata

--- src/app/novels/page.tsx
+++ src/app/novels/page.tsx
@@ -1,7 +1,17 @@
 import BreadCrumbs from '@/components/BreadCrumbs'
 import Footer from '@/components/Footer'
 import BooksList from '@/components/NovelsList'
+import { metaTitleTemplate } from '@/library/environment'
 import { getAllNovels } from '@/library/utilities/server'
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+	title: `All novels | ${metaTitleTemplate}`,
+	description: 'Browse our collection of classic novels for free online reading without ads or distractions.',
+	alternates: {
+		canonical: '/novels',
+	},
+}
 
 export default async function AllBooksPage() {
 	const novels = await getAllNovels()
