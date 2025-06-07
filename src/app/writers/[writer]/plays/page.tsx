8db1a1c5a9654aba60fe9597db2e43d212dbd1b6
Dan Edwards <daniel.edwards96@yahoo.com>
2025-06-07 20:49:38 +0100

    Add notFound in production

--- src/app/writers/[writer]/plays/page.tsx
+++ src/app/writers/[writer]/plays/page.tsx
@@ -1,4 +1,8 @@
+import { isProduction } from '@/library/environment'
+import { notFound } from 'next/navigation'
+
 export default function AuthorPlaysPage() {
+	if (isProduction) return notFound()
 	return (
 		<>
 			<h1>[Author]/plays/page.tsx</h1>
--- src/app/writers/[writer]/poems/page.tsx
+++ src/app/writers/[writer]/poems/page.tsx
@@ -1,4 +1,9 @@
+import { isProduction } from '@/library/environment'
+import { notFound } from 'next/navigation'
+
 export default function AuthorPoemsPage() {
+	if (isProduction) return notFound()
+
 	return (
 		<>
 			<h1>[Author]/poems/page.tsx</h1>
