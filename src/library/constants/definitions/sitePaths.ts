ad5a8fc041a9e9014b80a40fa7de8bce72339bdd
Dan Edwards <daniel.edwards96@yahoo.com>
2025-06-07 20:50:56 +0100

    Add sitePaths and dates

---
+++ src/library/constants/definitions/sitePaths.ts
@@ -0,0 +1,19 @@
+export const sitePaths: Array<{ path: string; hidden?: boolean }> = [
+	{
+		path: '/icon.svg',
+		hidden: true,
+	},
+	{ path: '/privacy-policy' },
+	{ path: '/about' },
+	{ path: '/novels' },
+	{ path: '/writers' },
+
+	{
+		path: '/writers/plays',
+		hidden: true,
+	},
+	{
+		path: '/writers/poems',
+		hidden: true,
+	},
+]
