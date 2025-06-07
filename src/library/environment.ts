544092781cdb65b15ce86048e57f22b9f25f3b4f
Dan Edwards <daniel.edwards96@yahoo.com>
2025-06-07 20:50:43 +0100

    Remove unused variables

--- src/library/environment.ts
+++ src/library/environment.ts
@@ -3,10 +3,8 @@ import type { LogLevel } from '@/types'
 export const isProduction = process.env.NODE_ENV === 'production'
 export const isDevelopment = process.env.NODE_ENV === 'development'
 
-export const siteIsLaunched = true
-export const freeBareDomain = 'classic-reader.vercel.app'
 export const customBareDomain = 'classicreader.org'
-export const productionBaseURL = `https://${siteIsLaunched ? customBareDomain : freeBareDomain}`
+export const productionBaseURL = `https://${customBareDomain}`
 export const developmentBaseURL = 'http://localhost:3000'
 export const dynamicBaseURL = isProduction ? productionBaseURL : developmentBaseURL
 
